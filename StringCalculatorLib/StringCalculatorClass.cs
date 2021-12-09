aca3c5d8958af4606c091cfe8d5c1fcb258d7c76
BhaveshJain2527 <89921365+BhaveshJain2527@users.noreply.github.com>
2021-12-09 22:31:56 +0530

    Update StringCalculatorClass.cs

--- StringCalculatorLib/StringCalculatorClass.cs
+++ StringCalculatorLib/StringCalculatorClass.cs
@@ -12,51 +12,22 @@ namespace StringCalculatorLib
         #endregion
 
         #region Methods
-
-        /// <summary>
-        /// Adds the specified numbers.
-        /// </summary>
-        /// <param name="numbers">The numbers.</param>
-        /// <returns></returns>
         public int Add(string numbers)
         {
-            int result = 0;
-
-            if(string.IsNullOrEmpty(numbers))
-            {
-                return 0;
-            }
-
+            if(string.IsNullOrEmpty(numbers)) return 0;
             var numberList = numbers.Split(_delimiters.ToArray(), StringSplitOptions.None).Select(int.Parse).ToList();
-
             bool IsNegativeExists = IsNegativeNumberExists(numberList);
-
-            if (!IsNegativeExists)
-            {
-                result = numberList.Where(x => x <= maxLimit).Sum();
-            }
-            return result;
+            return IsNegativeExists ? 0 : numberList.Where(x => x <= maxLimit).Sum();
         }
 
-        /// <summary>
-        /// Determines whether [is negative number exists] [the specified number list].
-        /// </summary>
-        /// <param name="numberList">The number list.</param>
-        /// <returns>
-        ///   <c>true</c> if [is negative number exists] [the specified number list]; otherwise, <c>false</c>.
-        /// </returns>
         public bool IsNegativeNumberExists(List<int> numberList)
         {
             if (!numberList.Any(x => x < 0)) return false;
-
             var negativeNumbers = string.Join(",", numberList.Where(x => x < 0).Select(x => x.ToString()).ToArray());
             Console.WriteLine("Negatives number are not allowed - " +negativeNumbers);
-
             return true;
-
         }
 
         #endregion
     }
 }
-
