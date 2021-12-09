fb3b57f97dcb23d07dc64070d77c21ec187bb91f
BhaveshJain2527 <89921365+BhaveshJain2527@users.noreply.github.com>
2021-12-09 22:14:26 +0530

    Updated => StringCalculatorLib with Add method and check for negative
    Not able to test because of following error:
    Error	NETSDK1064	Package xunit.analyzers, version 0.10.0 was not found. It might have been deleted since NuGet restore. Otherwise, NuGet restore might have only partially completed, which might have been due to maximum path length restrictions.	StringCalculatorLib.Tests	C:\Program Files\dotnet\sdk\5.0.401\Sdks\Microsoft.NET.Sdk\targets\Microsoft.PackageDependencyResolution.targets	241

--- StringCalculatorLib/StringCalculatorClass.cs
+++ StringCalculatorLib/StringCalculatorClass.cs
@@ -1,9 +1,62 @@
-﻿using System;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace StringCalculatorLib
 {
     public class StringCalculatorClass
     {
-        //Add Function Here
+        #region Properties
+        private List<string> _delimiters = new List<string> { ",", "\n" };
+        private const int maxLimit = 1000;
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Adds the specified numbers.
+        /// </summary>
+        /// <param name="numbers">The numbers.</param>
+        /// <returns></returns>
+        public int Add(string numbers)
+        {
+            int result = 0;
+
+            if(string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var numberList = numbers.Split(_delimiters.ToArray(), StringSplitOptions.None).Select(int.Parse).ToList();
+
+            bool IsNegativeExists = IsNegativeNumberExists(numberList);
+
+            if (!IsNegativeExists)
+            {
+                result = numberList.Where(x => x <= maxLimit).Sum();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Determines whether [is negative number exists] [the specified number list].
+        /// </summary>
+        /// <param name="numberList">The number list.</param>
+        /// <returns>
+        ///   <c>true</c> if [is negative number exists] [the specified number list]; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsNegativeNumberExists(List<int> numberList)
+        {
+            if (!numberList.Any(x => x < 0)) return false;
+
+            var negativeNumbers = string.Join(",", numberList.Where(x => x < 0).Select(x => x.ToString()).ToArray());
+            Console.WriteLine("Negatives number are not allowed - " +negativeNumbers);
+
+            return true;
+
+        }
+
+        #endregion
     }
 }
+
